2c756d422bf346884b90932e298cbd987efc93c0
Russoul <russoulmc@gmail.com>
2018-04-06 17:53:23 +0300

    deps

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # voxelized3d
 experimental 3D voxel engine
 
-depends on [FastNoise](https://github.com/Auburns/FastNoise), [GLFW3](http://www.glfw.org/), [GLAD](https://github.com/Dav1dde/glad), OpenGL
+depends on [FastNoise](https://github.com/Auburns/FastNoise), [GLFW3](http://www.glfw.org/), [GLAD](https://github.com/Dav1dde/glad), OpenGL and inner bindings library (compiled manually)
 
 ![UMDC + sphere + noise (radius displacement)](imgs/umdc_sphere_displacement.png)
 
---
+++ bindings/lib.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include "FastNoise.h"
+
+extern "C" {
+
+
+    void* allocFastNoise(){
+        FastNoise noise;
+        auto * ptr = static_cast<FastNoise *>(malloc(sizeof(FastNoise)));
+        *ptr = noise;
+        return ptr;
+    }
+
+    void freeFastNoise(void* noise){
+        free(noise);
+    }
+
+    void setFrequency(void* noise, FN_DECIMAL freq){
+        ((FastNoise*)noise)->SetFrequency(freq);
+    }
+
+    void setSeed(void* noise, int seed){
+        ((FastNoise*)noise)->SetSeed(seed);
+    }
+
+    void setNoiseType(void* noise, FastNoise::NoiseType type){
+        ((FastNoise*)noise)->SetNoiseType(type);
+    }
+
+    FN_DECIMAL getValue(void* noise, FN_DECIMAL x, FN_DECIMAL y, FN_DECIMAL z){
+        return ((FastNoise*)noise)->GetValue(x,y,z);
+    }
+
+    FN_DECIMAL getPerlin(void* noise, FN_DECIMAL x, FN_DECIMAL y, FN_DECIMAL z){
+        return ((FastNoise*)noise)->GetValue(x,y,z);
+    }
+};
